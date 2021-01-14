40fc064a74139bbdaae31b4e767b8fa4706de1d3
batyapollack <batyapollack6001@gmail.com>
2021-01-14 17:27:41 +0200

    Dynamic gallery

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+import Card from './Components/Card';
+
+
+
+
+const App = ()=>
+{
+  const [images, setImages] = useState([
+    {  url: `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`},
+    {  url: `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`},
+    {  url: `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`},
+    {  url: `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`},
+    {  url: `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`},
+    {  url: `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`}
+  ]) ;
+
+
+  const addImage = ()=>{
+    setImages([...images,  {  url: `https://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`}] );
+  }
+  const removeImage = ()=>{
+    setImages(images.slice(0,-1))
+  }
+
+  return (
+    <section className = "hero">
+      <div className="cardContainer">
+      <Card  images ={images} />
+      </div>
+
+      <div className="btnContainer">
+        <button onClick={removeImage}> Remove</button>
+        <button onClick={addImage}>Add</button>
+      </div>
+    </section>
+
+  );
+}
+
+export default App;
